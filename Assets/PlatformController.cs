0bf27c94c9cdd606b008f2c42a5f225fb50cfed1
WillBoudreau <williamboudreau804@gmail.com>
2024-02-05 15:52:06 -0500

    Created the player, collectable and instantiation

---
+++ Assets/PlatformController.cs
@@ -0,0 +1,34 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PlatformController : MonoBehaviour
+// // {
+// //     private int platformSpeed;
+// //     private int platformDistanceMaX = 45;
+// //     private int plaformDistanceMin = 0;
+// //     public int interpolationFramesCount = 45; // Number of frames to completely interpolate between the 2 positions
+// //     int elapsedFrames = 0;
+// //     // Start is called before the first frame update
+// //     void Start()
+// //     {
+        
+// //     }
+
+// //     // Update is called once per frame
+    
+// //     void Update()
+// //     {
+// //         float interpolationRatio = (float)elapsedFrames / interpolationFramesCount;
+
+// //         Vector3 interpolatedPosition = Vector3.Lerp(plaformDistanceMin,platformDistanceMaX, interpolationRatio);
+
+// //         //elapsedFrames = (elapsedFrames + 1) % (interpolationFramesCount + 1);  // reset elapsedFrames to zero after it reached (interpolationFramesCount + 1)
+
+// //     }
+// //     void PlatformSlider()
+// //     {
+
+// //     }
+    
+// //}
---
+++ Assets/PlayerMovementController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField]  private float speed = 12f;
+    [SerializeField]  private float gravity = -9.81f;
+    [SerializeField]  private int MinY = -15;
+    [SerializeField]  private Vector3 positionY;
+    [SerializeField]  private CharacterController CharacterCont;
+    [SerializeField]  private float jumpHeight = 3f;
+    [SerializeField]  private Transform groundCheck;
+    [SerializeField]  private GameObject Bullet;
+    public float groundDist = 0.4f;
+    public LayerMask GroundMask;
+    bool IsGrounded;
+    Vector3 velocity;
+    // Update is called once per frame
+    void Update()
+    {
+        IsGrounded = Physics.CheckSphere(groundCheck.position,groundDist,GroundMask);
+
+        if(IsGrounded && velocity.y < 0)
+        {
+            velocity.y = -2.0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        CharacterCont.Move(move * Time.deltaTime * speed);
+        
+        velocity.y += gravity * Time.deltaTime;
+
+        CharacterCont.Move(velocity * Time.deltaTime * speed);
+        //Jump if spacebar is pressed
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        //Fire bullets
+        if(Input.GetMouseButtonDown(0))
+        {
+           Instantiate(Bullet,transform.position,transform.rotation);
+           Destroy(Bullet,5f);
+        }
+        //Crounch
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("Crouch");
+        }
+        //Pause
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0.0f;
+        }
+        //Kill box
+        if(velocity.y <= -15)
+        {
+            velocity.y = 0;
+        }
+        void OnTriggerEnter(Collision collision)
+        {
+            //Collectable pickup
+            if(collision.gameObject.CompareTag("Collectable"))
+            {
+                Debug.Log("Hi");
+            }
+        }
+    }
+}
